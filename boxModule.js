8ff93cb122410d309fce69cc6da74b46fb0cb707
rek <rekarnar@gmail.com>
2012-09-30 15:32:44 +0545

    code tidyups

--- boxModule.js
+++ boxModule.js
@@ -3,46 +3,64 @@
 
 "use strict";
 var boxModule = (function ($) {
-  var _this, _dom, _boxName = 'main',
+  var _this, _dom, _options = {
+    boxName: 'main',
+    widthDiv: '',
+    colorDiv: ''
+  },
     pub = {},
 
-    changeSize = function (b) {
-      b.css('width', $('#size').val());
-    },
-
-    changeColor = function (b) {
-      b.css('background', $('#color').val());
+    /**
+    * Loop through the css fields given
+    * and display them in the given divs
+    */
+    setDivValues = function (obj, vals) {
+      $.each(vals, function (key, value) {
+        $('#' + value).text(obj.css(key));
+      });
     },
 
     /**
-     * When the box is clicked, update the display and
-     * save the this object to the dom element
+     * When the box is clicked, update the display
+     * and save the this object to the dom element
      *
      */
     addClickDisplay = function (d) {
       d.click(function (e) {
-        $('#size_color').text($(e.target).css('background-color'));
+        // display the values when clicked
+        setDivValues($(e.target), {
+          'background-color': _options.colorDiv,
+          'width': _options.widthDiv
+        });
+        // save this node to the dom
         $('#loaded').data('node', _this);
       });
     },
 
     setup = function () {
-      changeSize(_dom);
-      changeColor(_dom);
       addClickDisplay(_dom);
-      return this;
+      _dom.css('width', $('#size').val());
+      _dom.css('background', $('#color').val());
+
+      return _this;
     };
 
-  pub.create = function () {
+  pub.create = function (options) {
+    // make the object
     _dom = $('<div class="box"></div>');
+    // save a self refrence of ourselves
     _this = this;
-    $('#' + _boxName).append(_dom);
+    // save the new options
+    pub.options(options);
+    // and draw it on the page
+    $('#' + _options.boxName).append(_dom);
 
+    // set the drawn box to our options
     return setup();
   };
 
-  pub.changeDiv = function (newName) {
-    _boxName = newName;
+  pub.options = function (options) {
+    $.extend(_options, options);
   };
 
   pub.del = function () {
--- tutorial1.js
+++ tutorial1.js
@@ -3,36 +3,72 @@
 $(function () {
   "use strict";
 
+  var cDiv = 'option_color',
+    wDiv = 'option_width';
+
+  /**
+   * When delete is pressed
+   * get the node from the dom element
+   * and delete it where ever it is
+   */
   $('#delete').on('click', function () {
     var node = $('#loaded').data('node');
     if (node) {
       console.log('Node exists');
       node.del();
       $('#loaded').data('node', null);
-
+      resetSidebar();
     } else {
       console.log('Node is not loaded');
     }
   });
 
+  /**
+   * When the add button is pressed
+   * clear the form fields and reset focus
+   * then create the box, setting some options
+   */
   $('#add').on('click', function () {
-    boxModule.create();
+    resetForm();
+
+    boxModule.create({
+      'colorDiv': cDiv,
+      'widthDiv': wDiv
+    });
+
   });
 
+  /**
+   * When enter is pressed in the size field
+   * move the focus to the focus field
+   */
   $('#size').keypress(function (e) {
     if (e.which === 13) {
       $('#color').focus();
     }
   });
 
+  /**
+   * When enter is pressed in the color box
+   * then simulate a click on the add button
+   */
   $('#color').keypress(function (e) {
     if (e.which === 13) {
       $('#add').click();
-      $(this).val('');
-      $('#size').val('');
-      $('#size').focus();
     }
   });
 
+  function resetForm() {
+    $('#color').val('');
+    $('#size').val('');
+    $('#size').focus();
+  }
+
+  function resetSidebar(){
+    $('#'+wDiv).text('');
+    $('#'+cDiv).text('');
+  }
+
+  // on page load, focus the size field
   $('#size').focus();
 });
